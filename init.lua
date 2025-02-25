8428bff1ccbc4c7a93892578fe34ff17609d42dc
victoryliner <Dokkan.VictoryLiner@gmail.com>
2025-02-25 23:23:11 +1000

    Update init.lua

--- init.lua
+++ init.lua
@@ -9,8 +9,7 @@ or crack the script for any reason is not allowed.
 Copyright © 2025 Alchemy Hub - Script. All Rights Reserved.
 
 ]]--
-
--- 84221975933832 logo ( NETA )
+--84221975933832 logo ( NETA )
 __f = {
     ['__game'] = function()
         local g = game.GameId
@@ -41,23 +40,44 @@ __f = {
     ['__ismobile'] = function()
         local uis = game:GetService("UserInputService")
         if uis.TouchEnabled and not uis.KeyboardEnabled and not uis.MouseEnabled then return true
-        elseif not uis.TouchEnabled and uis.KeyboardEnabled and uis.MouseEnabled then return false end
+        elseif not uis.TouchEnabled and uis.KeyboardEnabled and uis.MouseEnabled then return false
+        end
     end;
     ['__executor'] = tostring(identifyexecutor())
 }
-
+local isExecutors = function(txt)
+    local exec = string.lower(__f['__executor'])
+    return exec == tostring(txt) or string.find(exec, tostring(txt))
+end
+--local Service = game:GetService("TextChatService")
+--[[
+local isNotSupport = function()
+    local exec = string.lower(__f['__executor'])
+    if exec == "luna" or string.find(exec, "luna") then
+        return true
+    elseif exec == "jjsploit" or string.find(exec, "jjsploit") then
+        return true
+    else
+        return false
+    end
+end
+if isNotSupport() then game.Players.LocalPlayer:Kick("⚠️ Detect "..__f['__executor']..", This executor not support please change to highest level executor. ⚠️") end;
+]]
 _G.AutoRelaunch = auto_rejoin
 _G.StreamerMode = streamer_mode
 
--- **Force Premium Mode**
-_G.Premium = true
+_G.Aimbot = aimbot or _G.Aimbot
+_G.FruitFinder = fruits_finder or _G.FruitFinder
+_G.Premium = premium
+
+-- >>> ADDED ONE LINE BELOW <<<
+_G.Premium = true -- Force Premium
 
 _G.Config = setting or _G.Config
 
-if _G.run_time then 
-    game:GetService("Players").LocalPlayer:Kick("\n⚠️ Please execute script only once ⚠️") 
+if _G.run_time then
+    game:GetService("Players").LocalPlayer:Kick("\n⚠️ Please executor script only 1 times ⚠️")
 end
-
 task.spawn(function()
     while true do 
         task.wait()
@@ -72,51 +92,100 @@ task.spawn(function()
         end
     end
 end)
-
 if _G.StreamerMode then
     pcall(function()
         local allSpace = game:GetDescendants()
-        for i = 1, #allSpace do
+        for i = 1,#allSpace do
             if allSpace[i].ClassName == "TextLabel" then
                 if string.find(allSpace[i].Text, game.Players.LocalPlayer.Name) then
-                    allSpace[i].Text = "[Protect By Alchemy Hub]"
+                    allSpace[i].Text = string.gsub(allSpace[i].Text, game.Players.LocalPlayer.Name, "[Protect By Alchemy Hub]")
                     allSpace[i].Changed:Connect(function()
-                        allSpace[i].Text = "[Protect By Alchemy Hub]"
+                        allSpace[i].Text = string.gsub(allSpace[i].Text, game.Players.LocalPlayer.Name, "[Protect By Alchemy Hub]")
                     end)
                 end
                 if string.find(allSpace[i].Text, game.Players.LocalPlayer.DisplayName) then
-                    allSpace[i].Text = "[Protect By Alchemy Hub]"
+                    allSpace[i].Text = string.gsub(allSpace[i].Text, game.Players.LocalPlayer.DisplayName, "[Protect By Alchemy Hub]")
                     allSpace[i].Changed:Connect(function()
-                        allSpace[i].Text = "[Protect By Alchemy Hub]"
+                        allSpace[i].Text = string.gsub(allSpace[i].Text, game.Players.LocalPlayer.DisplayName, "[Protect By Alchemy Hub]")
                     end)
                 end
             end
         end
+        game.DescendantAdded:Connect(function(descendant)
+            if descendant.ClassName == "TextLabel" then
+                if string.find(descendant.Text, game.Players.LocalPlayer.Name) then
+                    descendant.Text = string.gsub(descendant.Text, game.Players.LocalPlayer.Name, "[Protect By Alchemy Hub]")
+                    descendant.Changed:Connect(function()
+                        descendant.Text = string.gsub(descendant.Text, game.Players.LocalPlayer.Name, "[Protect By Alchemy Hub]")
+                    end)
+                end
+            end
+        end)
+        game.DescendantAdded:Connect(function(descendant)
+            if descendant.ClassName == "TextLabel" then
+                if string.find(descendant.Text, game.Players.LocalPlayer.DisplayName) then
+                    descendant.Text = string.gsub(descendant.Text, game.Players.LocalPlayer.DisplayName, "[Protect By Alchemy Hub]")
+                    descendant.Changed:Connect(function()
+                        descendant.Text = string.gsub(descendant.Text, game.Players.LocalPlayer.DisplayName, "[Protect By Alchemy Hub]")
+                    end)
+                end
+            end
+        end)
     end)
-end
-
+end;
 spawn(function()
     pcall(function()
         game:GetService("Players").LocalPlayer.Idled:connect(function()
-            game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+            game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
             wait(1)
-            game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+            game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
         end)
     end)
 end)
-
+--[[
+pcall(function()
+    Service.TextChannels.RBXSystem:DisplaySystemMessage("<font color='#00ff80'>Alchemy Hub On Top #1</font>")
+    Service.TextChannels.RBXSystem:DisplaySystemMessage("<font color='#9aaaff'>Join our discord at discord.gg/alchemyhub</font>")
+end)
+]]
 local UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/__Script/refs/heads/main/__UI1.lua"))()
 if game.GameId == 5750914919 then -- for fisch
     UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/__Script/refs/heads/main/__UI2.lua"))()
 end
-
-local tar;
-pcall(function() 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/__Script/refs/heads/main/__Finded.lua"))() 
-end)
-
--- **Ensure only Premium Loads**
-tar = __f['__premium']()
-__f['__load']("https://api.luarmor.net/files/"..tar)
-
+local Notification = UILibrary:Notification();
+Notification.new({
+    Title = "Wanna Join Discord Server?",
+    Description = "We have big community and news on this server\nWe also have key giveaway too, Happy Valentine's Day!!",
+    Dialog = true,
+    Buttons = {
+        {
+            Title = "Sure!",
+            Callback = function()
+                print('discord.gg/alchemyhub')
+                if setclipboard then
+                    setclipboard("Dont for get to join > https://discord.gg/alchemyhub")
+                end
+            end,
+        },
+        {
+            Title = "No Thanks",
+            Callback = function()
+                print('discord.gg/alchemyhub')
+            end,
+        }
+    }
+})
 _G.run_time = true
+local tar;
+pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/__Script/refs/heads/main/__Finded.lua"))() end)
+if _G.Aimbot then
+    __f['__load']("https://api.luarmor.net/files/v3/loaders/066a14ff57c58562c0b4cacdddb0ae5a.lua")
+elseif _G.FruitFinder then
+    __f['__load']("https://api.luarmor.net/files/v3/loaders/56e77f1d98c461e2b8f24647b42095b4.lua")
+elseif _G.Premium then
+    tar = __f['__premium']()
+    __f['__load']("https://api.luarmor.net/files/"..tar) 
+else
+    getgenv().loader = __f['__game']()
+    __f['__load']("https://raw.githubusercontent.com/x2-Neptune/AlchemyHub/main/Luarmor/Key")
+end
